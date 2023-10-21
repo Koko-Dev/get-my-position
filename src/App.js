8749fbc75b1edc00c13f83d15bd53817e5f8c327
Koko-Dev <koko.webdev@gmail.com>
2023-10-21 09:48:43 -0400

    App.js:  Create four pieces of state for loading, countClicks, position and error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+
+
 export default function App() {
+  const [ isLoading, setIsLoading ] = useState( false );
+  const [ countCicks, setCountClicks ] = useState( 0 );
+  const [ position, setPosition ] = useState( {} );
+  const [ error, setError ] = useState( null );
+  
   return (
       <div>
         <h1>App</h1>
