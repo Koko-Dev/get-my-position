058aa1f73d072be6d7a9ab162967ef324e538e65
Koko-Dev <koko.webdev@gmail.com>
2023-10-21 10:13:37 -0400

    App.js:  Add a button some jsx conditionals and elements;  these will not be plugged in yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import { useState } from 'react';
 
 
+function getGeolocation() {
+  
+}
+
 export default function App() {
   const [ isLoading, setIsLoading ] = useState( false );
   const [ countCicks, setCountClicks ] = useState( 0 );
   const [ position, setPosition ] = useState( {} );
   const [ error, setError ] = useState( null );
   
+  const { lat, lng } = position;
+  
+  
   return (
       <div>
-        <h1>App</h1>
+        <div>
+          <button
+              disabled={ isLoading }
+          >Get my position
+          </button>
+          
+          { isLoading && <p>Loading position... </p> }
+          { error && <p>(error) </p> }
+          { isLoading && !error && lat && lng && (
+              <p>
+                Your GPS position:{ ' ' }
+                <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href={ `https://www.openstreetmap.org/#map=16/${ lat }/${ lng }` }
+                >
+                  { lat }, { lng }
+                </a>
+              </p> ) }
+          
+          <p>You requested position { countCicks } times</p>
+        </div>
       </div> );
 }
