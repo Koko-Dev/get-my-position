8edfba4691b231812bda8a0ba254c9067cc82c59
Koko-Dev <koko.webdev@gmail.com>
2023-10-21 11:34:26 -0400

    Complete geolocation functionality and add counterclic ks

--- src/App.js
+++ src/App.js
@@ -13,30 +13,59 @@ export default function App() {
   
   const { lat, lng } = position;
   
+  function getPosition() {
+    setCountClicks( ( count ) => count + 1 );
+    if ( !navigator.geolocation ) return setError( 'Your browser dow not support geolocation' );
+    
+    setIsLoading( true );
+    
+    navigator.geolocation.getCurrentPosition( ( pos ) => {
+      setPosition( {lat: pos.coords.latitude, lng: pos.coords.longitude} );
+      setIsLoading( false );
+      },
+                                              ( error ) => {
+      setError( error.message );
+      setIsLoading( false );
+    } );
+    
+    /*
+    
+      function success( pos ) {
+         setPosition( { lat: pos.coords.latitude, lng: pos.coords.longitude } );
+         setIsLoading( false );
+       }
+     
+     function error( err ) {
+       setError( err.message );
+       setIsLoading( false );
+     }
+     
+     navigator.geolocation.getCurrentPosition( success, error );
+     
+     */
+    
+  }
   
   return (
       <div>
-        <div>
-          <button
-              disabled={ isLoading }
-          >Get my position
-          </button>
-          
-          { isLoading && <p>Loading position... </p> }
-          { error && <p>(error) </p> }
-          { isLoading && !error && lat && lng && (
-              <p>
-                Your GPS position:{ ' ' }
-                <a
-                    target='_blank'
-                    rel='noreferrer'
-                    href={ `https://www.openstreetmap.org/#map=16/${ lat }/${ lng }` }
-                >
-                  { lat }, { lng }
-                </a>
-              </p> ) }
-          
-          <p>You requested position { countCicks } times</p>
-        </div>
+        <button onClick={ getPosition } disabled={ isLoading }>
+          Get my position
+        </button>
+        
+        { isLoading && <p>Loading position...</p> }
+        { error && <p>{ error }</p> }
+        { !isLoading && !error && lat && lng && (
+            <p>
+              Your GPS position:{ ' ' }
+              <a
+                  target='_blank'
+                  rel='noreferrer'
+                  href={ `https://www.openstreetmap.org/#map=16/${ lat }/${ lng }` }
+              >
+                { lat }, { lng }
+              </a>
+            </p> ) }
+        
+        <p>You requested position { countCicks } times</p>
       </div> );
 }
